7f354776029a463d62a5b902639cc5341519bdd6
Thierry Fournier <tfournier@arpalert.org>
2022-07-04 22:04:23 +0200

    MINOR: Add gen code tool in the tests

--- radix_binary_test.go
+++ radix_binary_test.go
@@ -2,6 +2,7 @@
 
 package radix
 
+import "fmt"
 import "testing"
 
 func TestBitcmp(t *testing.T) {
@@ -126,4 +127,52 @@ func TestBitcmp(t *testing.T) {
 	if res != 23 {
 		t.Errorf("Expect 23, got %d", res)
 	}
+}
+
+func TestGenMasks(t *testing.T) {
+	var i int
+	var j int
+	var beg_mask [8]byte
+	var end_mask [8]byte
+	var mix_mask [8][8]byte
+
+	/* Build mask value for byte keep at end of byte */
+	for i = 0; i < 8; i++ {
+		beg_mask[i] = 0xff >> i
+	}
+
+	/* Build values for byte keepv at start of byte */
+	for i = 0; i < 8; i++ {
+		end_mask[i] = 0xff << (7 - i)
+	}
+
+	/* Build values for mixed mask */
+	for  i = 0; i < 8; i++ {
+		for j = 0; j < 8; j++ {
+			mix_mask[i][j] = beg_mask[i] & end_mask[j]
+		}
+	}
+
+	/* Display results */
+	fmt.Printf("var beg_mask = [8]byte{ 0x%02x", beg_mask[0])
+	for i = 1; i < 8; i++ {
+		fmt.Printf(", 0x%02x", beg_mask[i])
+	}
+	fmt.Printf(" }\n")
+
+	fmt.Printf("var end_mask = [8]byte{ 0x%02x", end_mask[0])
+	for i = 1; i < 8; i++ {
+		fmt.Printf(", 0x%02x", end_mask[i])
+	}
+	fmt.Printf(" }\n")
+
+	fmt.Printf("var mix_mask = [8][8]byte{\n");
+	for i = 0; i < 8; i++ {
+		fmt.Printf("\t{ 0x%02x", mix_mask[i][0])
+		for j = 1; j < 8; j++ {
+			fmt.Printf(", 0x%02x", mix_mask[i][j])
+		}
+		fmt.Printf(" },\n")
+	}
+	fmt.Printf(" }\n")
 }
